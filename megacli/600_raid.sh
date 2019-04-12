3253dd86bf758b5475e82f625a6a892968f8e251
柴向停 <cxt@tom.com>
2019-04-12 23:27:13 +0800

    add megacli.pdis

---
+++ megacli/600_raid.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ ! -f /usr/bin/MegaCli64 ]; then ln -s /opt/MegaRAID/MegaCli/MegaCli64 /usr/bin/MegaCli64; fi
+
+d="	"
+
+function sysoOne(){
+	echo -e "${d}{ 
+		'metric': '${1}', 
+		'endpoint': '', 
+		'timestamp': `date +"%s"`,
+		'step': 60
+		'value': ${2},
+		'counterType': 'GAUGE',
+		'tags': '${3}'
+	}\c"
+}
+
+MegaCli -PDGetNum -aALL -Silent -NoLog
+pdiskcount=$?
+pdiskok=$(MegaCli -PDList -aALL -NoLog | grep "Firmware state" | grep Online | wc -l)
+echo "["
+sysoOne "megacli.pdisk" $(expr $pdiskcount - $pdiskok)
+echo ""
+echo "]"
