6cfe9311e28444d72cc80732b99a51afb389accc
柴向停 <cxt@tom.com>
2019-04-13 03:39:15 +0800

    change to one line

--- megacli/600_raid.sh
+++ megacli/600_raid.sh
@@ -2,16 +2,15 @@
 
 if [ ! -f /usr/bin/MegaCli64 ]; then ln -s /opt/MegaRAID/MegaCli/MegaCli64 /usr/bin/MegaCli64; fi
 
-d="	"
+d=""
 
 function sysoOne(){
 	echo -e "${d}{'metric': '${1}', 'endpoint': '', 'timestamp': `date +"%s"`, 'step': 600,	'value': ${2}, 'counterType': 'GAUGE','tags': '${3}'}\c"
 }
 
-MegaCli -PDGetNum -aALL -Silent -NoLog
+MegaCli -PDGetNum -aALL -Silent -NoLog > /dev/null
 pdiskcount=$?
 pdiskok=$(MegaCli -PDList -aALL -NoLog | grep "Firmware state" | grep Online | wc -l)
-echo "["
+echo -e "[\c"
 sysoOne "megacli.pdisk" $(expr $pdiskcount - $pdiskok)
-echo ""
 echo "]"
